445730baa88f7cdccb9ead394803ef9045d87d32
Noa Nordén <noa.norden@gmail.com>
2025-10-24 11:24:32 +0200

    Endret navn fra Oppgave1 til Luftfartshinder, diverse CSS

---
+++ Luftfartshinder/Luftfartshinder/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Luftfartshinder.Models;
+
+namespace Luftfartshinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DataForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DataForm(ObstacleData obstacledata)
+        {
+            return View("Overview", obstacledata);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Oppgave1/Oppgave1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Oppgave1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
