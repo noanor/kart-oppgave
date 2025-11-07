e972a6d51ef4cd53ccc06cb5455aad2f0b105f7b
Madalitso <madalitsoskjelnes@hotmail.com>
2025-11-07 19:45:09 +0100

    Gir superAdmin tilgang til Slette og rediger ebrukere

---
+++ Luftfartshinder/Luftfartshinder/Controllers/SuperAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luftfartshinder.Controllers
+{
+    public class SuperAdminController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Luftfartshinder/Luftfartshinder/Repository/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Luftfartshinder.DataContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Luftfartshinder.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthDbContext authDbContext;
+
+        public UserRepository(AuthDbContext authDbContext)
+        {
+            this.authDbContext = authDbContext;
+        }
+        public async Task<IEnumerable<IdentityUser>> GetAll()
+        {
+            var users = await authDbContext.Users.ToListAsync();
+
+            var superAdminUser = await authDbContext.Users
+                .FirstOrDefaultAsync(x => x.Email == "superadmin@kartverket.no");
+
+            if (superAdminUser != null)
+            {
+                users.Remove(superAdminUser);
+            }
+            return users;
+        }
+    }
+}
