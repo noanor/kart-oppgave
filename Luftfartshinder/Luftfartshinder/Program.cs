001294d3dc19ebe21c9e862d5b2c5a96c82c7f9d
Madalitso <madalitsoskjelnes@hotmail.com>
2025-11-09 19:05:41 +0100

    små endring

--- Luftfartshinder/Luftfartshinder/Program.cs
+++ Luftfartshinder/Luftfartshinder/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddScoped<IDataRepocs, ObstacleDataRepo>();
 
 builder.Services.AddDbContext<ApplicationContext>(options =>
