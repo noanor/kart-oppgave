857910b09d6929de5907db348f886e5b74818567
denniit <dennis.tea@outlook.com>
2025-11-13 17:31:47 +0100

    Final touch

--- Luftfartshinder/Luftfartshinder/Models/ViewModel/RegisterViewModel.cs
+++ Luftfartshinder/Luftfartshinder/Models/ViewModel/RegisterViewModel.cs
@@ -4,10 +4,13 @@ namespace Luftfartshinder.Models.ViewModel
 {
     public class RegisterViewModel
     {
+        
         [Required(ErrorMessage = "First name is required")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "First name must be between 1 and 30 characters")]
         public string FirstName { get; set; }
         
         [Required(ErrorMessage = "Last name is required")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Last name must be between 1 and 30 characters")]
         public string LastName { get; set; }
         
         [Required(ErrorMessage = "Email is required")]
@@ -15,13 +18,18 @@ namespace Luftfartshinder.Models.ViewModel
         public string Email  { get; set; }
         
         [Required(ErrorMessage = "Username is required")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 30 characters")]
         public string Username { get; set; }
         
         [Required(ErrorMessage = "Password is required")]
-        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
         public string Password { get; set; }
         
         [Required(ErrorMessage = "Please select a role")]
         public string SelectedRole {get; set;}
+        
+        public string? Organization { get; set; }
+        
+        public string? OtherOrganization { get; set; }
     }
 }
