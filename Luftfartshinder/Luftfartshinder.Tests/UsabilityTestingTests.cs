ff66f2d3194c35eb118ea7cff6dcd84f079a982f
Madalitso <madalitsoskjelnes@hotmail.com>
2025-11-23 00:41:57 +0100

    Lag til de 4 forskjellige testene, og lekt litt med CSS på siden(nav-bar)

---
+++ Luftfartshinder/Luftfartshinder.Tests/UsabilityTestingTests.cs
@@ -0,0 +1,27 @@
+using Luftfartshinder.Controllers;
+using Luftfartshinder.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Luftfartshinder.Tests
+{
+    public class UsabilityTestingTests
+    {
+        [Fact]
+        public void Usability_PrivacyPage_ReturnsView()
+        {
+            // Arrange
+            var obstacleRepositoryMock = new Mock<IObstacleRepository>();
+            var controller = new HomeController(obstacleRepositoryMock.Object);
+
+            // Act
+            var result = controller.Privacy();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+        }
+    }
+}
+
