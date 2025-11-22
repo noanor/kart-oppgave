446fc50e8d5334a5a2da356ccd9cb35b492f0ea7
Madalitso <madalitsoskjelnes@hotmail.com>
2025-11-22 13:24:21 +0100

    Laget kartverket Logo og navn

--- Luftfartshinder/Luftfartshinder/obj/Debug/net9.0/Luftfartshinder.AssemblyInfo.cs
+++ Luftfartshinder/Luftfartshinder/obj/Debug/net9.0/Luftfartshinder.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Luftfartshinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf30041644ba669d5fdc2b896ba64c5fa584ec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24bc07fbe9835239b334c5499cc1e13c1f0b7ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Luftfartshinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Luftfartshinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
